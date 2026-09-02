e7b071ef033cc0f5a7851ea9323d8f59f0d533b8
Ktosox <ktoso3@gmail.com>
2026-09-02 14:23:22 +0200

    made card movement a bit smoother + more Slot work

--- game/test/test_slot_stuff/test_line_connector.gd
+++ game/test/test_slot_stuff/test_line_connector.gd
@@ -8,6 +8,10 @@ signal got_clicked(connector : Node2D)
 
 var drag_follow_mouse = false
 
+@export var has_connection = false
+
+@export var can_accept_connection = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,11 +23,38 @@ func _process(delta: float) -> void:
 		$DragLine.set_point_position(1,get_local_mouse_position())
 	pass
 
+func _input(event: InputEvent) -> void:
+	if event.is_class("InputEventMouseButton") and !event.is_pressed():
+		end_line_drag()
+
+func show_connection_availability():
+	if can_accept_connection:
+		$ConnectorBase.modulate = Color("Green")
+	else:
+		$ConnectorBase.modulate = Color("Red")
+	pass
+	
+func hide_connection_availability():
+	$ConnectorBase.modulate = Color("White")
 
 
+
+func accept_connection():
+	can_accept_connection = false
+	has_connection = true
+	pass
+
+func cancel_connection():
+	can_accept_connection = true
+	has_connection = false
+	pass
 
 func start_line_drag():
 	drag_follow_mouse = true
+	var other_connectors = get_tree().get_nodes_in_group("connector")
+	other_connectors.erase(self)
+	for connector in other_connectors:
+		connector.show_connection_availability()
 	pass
 
 func end_line_drag(global_pos = Vector2.INF):
@@ -32,6 +63,11 @@ func end_line_drag(global_pos = Vector2.INF):
 		$DragLine.set_point_position(1,global_pos)
 	else:
 		$DragLine.set_point_position(1,Vector2.ZERO)
+		
+	var other_connectors = get_tree().get_nodes_in_group("connector")
+	other_connectors.erase(self)
+	for connector in other_connectors:
+		connector.hide_connection_availability()
 	pass
 
 
@@ -40,5 +76,7 @@ func end_line_drag(global_pos = Vector2.INF):
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_class("InputEventMouseButton") and event.is_pressed():
-		got_clicked.emit(self)
+		if !has_connection:
+			start_line_drag()
+		#got_clicked.emit(self)
 	pass # Replace with function body.
